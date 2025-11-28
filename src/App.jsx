c2684eab4dec333382fe38d1d28e159f18fa634a
Vong sokpheak <vongsokpheak4@gmail.com>
2025-11-28 14:42:13 +0700

    update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { initializeApp } from 'firebase/app';
 import { getDatabase, ref, onValue } from 'firebase/database';
 import { getAuth, signInAnonymously } from 'firebase/auth';
